405205fea654a79371d04a593edf0c09d852bfcd
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-02 14:06:16 +0530

    2nd Aug Updated Code

--- app/src/main/java/online/internship/SignupActivity.java
+++ app/src/main/java/online/internship/SignupActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -13,6 +20,13 @@ public class SignupActivity extends AppCompatActivity {
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     EditText name,email,contact, password,confirmPassword;
+
+    //RadioButton male,female;
+    RadioGroup gender;
+
+    Spinner city;
+    //String[] cityArray = {"Ahmedabad","Vadodara","Surat","Rajkot","Gandhinagar","Kalol","Kadi","Mehsana","Dahod","Bharuch","Veraval","Ahmedabad","Vadodara","Surat","Rajkot","Gandhinagar","Kalol","Kadi","Mehsana","Dahod","Bharuch","Veraval"};
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,73 @@ public class SignupActivity extends AppCompatActivity {
         contact = findViewById(R.id.signup_contact);
         password = findViewById(R.id.signup_password);
         confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        city = findViewById(R.id.signup_city);
+
+        arrayList = new ArrayList<>();
+
+        /*for(int i=0;i<20;i++){
+            arrayList.add("Index "+i);
+        }*/
+
+        arrayList.add("Select City");
+        arrayList.add("Gandhinagar");
+        arrayList.add("Rajkot");
+        arrayList.add("Ahmedabad");
+        arrayList.add("Demo");
+        arrayList.add("XYZ");
+        arrayList.add("Surat");
+
+        arrayList.remove(3);
+        arrayList.set(3,"Vadodara");
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this,android.R.layout.simple_list_item_1,arrayList);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_activated_1);
+        city.setAdapter(adapter);
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==0){
+
+                }
+                else {
+                    new CommonMethod(SignupActivity.this, arrayList.get(i));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        gender = findViewById(R.id.signup_gender);
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton radioButton = findViewById(i); //i = R.id.signup_male,R.id.signup_female;
+                new CommonMethod(SignupActivity.this,radioButton.getText().toString());
+            }
+        });
+
+        /*male = findViewById(R.id.signup_male);
+        female = findViewById(R.id.signup_female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CommonMethod(SignupActivity.this,male.getText().toString());
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CommonMethod(SignupActivity.this,female.getText().toString());
+            }
+        });*/
 
         signup = findViewById(R.id.signup_signup);
         login = findViewById(R.id.signup_login);
