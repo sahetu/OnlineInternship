49db545683edb51d5654177d565a5b705fc711e3
Sagar Khatri <khatrisagar2@gmail.com>
2023-08-03 13:34:34 +0530

    3rd Aug Updated Code

--- app/src/main/java/online/internship/SignupActivity.java
+++ app/src/main/java/online/internship/SignupActivity.java
@@ -2,24 +2,30 @@ package online.internship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SignupActivity extends AppCompatActivity {
 
     Button login, signup;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-    EditText name,email,contact, password,confirmPassword;
+    EditText name, email, contact, password, confirmPassword, dob;
 
     //RadioButton male,female;
     RadioGroup gender;
@@ -27,6 +33,9 @@ public class SignupActivity extends AppCompatActivity {
     Spinner city;
     //String[] cityArray = {"Ahmedabad","Vadodara","Surat","Rajkot","Gandhinagar","Kalol","Kadi","Mehsana","Dahod","Bharuch","Veraval","Ahmedabad","Vadodara","Surat","Rajkot","Gandhinagar","Kalol","Kadi","Mehsana","Dahod","Bharuch","Veraval"};
     ArrayList<String> arrayList;
+    Calendar calendar;
+
+    String sCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,44 @@ public class SignupActivity extends AppCompatActivity {
         contact = findViewById(R.id.signup_contact);
         password = findViewById(R.id.signup_password);
         confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        dob = findViewById(R.id.signup_dob);
+
+        calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateClick = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(Calendar.YEAR, i);
+                calendar.set(Calendar.MONTH, i1);
+                calendar.set(Calendar.DAY_OF_MONTH, i2);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                dob.setText(sdf.format(calendar.getTime()));
+
+            }
+        };
+
+        dob.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(SignupActivity.this, dateClick, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+                //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+                return true;
+            }
+        });
+
+        /*dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(SignupActivity.this,dateClick,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+                //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });*/
 
         city = findViewById(R.id.signup_city);
 
@@ -56,20 +103,20 @@ public class SignupActivity extends AppCompatActivity {
         arrayList.add("Surat");
 
         arrayList.remove(3);
-        arrayList.set(3,"Vadodara");
+        arrayList.set(3, "Vadodara");
 
-        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this,android.R.layout.simple_list_item_1,arrayList);
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1, arrayList);
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_activated_1);
         city.setAdapter(adapter);
 
         city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i==0){
-
-                }
-                else {
-                    new CommonMethod(SignupActivity.this, arrayList.get(i));
+                if (i == 0) {
+                    sCity = "";
+                } else {
+                    sCity = arrayList.get(i);
+                    new CommonMethod(SignupActivity.this, sCity);
                 }
             }
 
@@ -85,7 +132,7 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 RadioButton radioButton = findViewById(i); //i = R.id.signup_male,R.id.signup_female;
-                new CommonMethod(SignupActivity.this,radioButton.getText().toString());
+                new CommonMethod(SignupActivity.this, radioButton.getText().toString());
             }
         });
 
@@ -118,31 +165,34 @@ public class SignupActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(name.getText().toString().trim().equals("")){
+                if (name.getText().toString().trim().equals("")) {
                     name.setError("Name Required");
-                }
-                else if (email.getText().toString().trim().equals("")) {
+                } else if (email.getText().toString().trim().equals("")) {
                     email.setError("Email Id Required");
                 } else if (!email.getText().toString().trim().matches(emailPattern)) {
                     email.setError("Valid Email Id Required");
-                }
-                else if(contact.getText().toString().trim().equals("")){
+                } else if (contact.getText().toString().trim().equals("")) {
                     contact.setError("Contact No. Required");
-                }
-                else if(contact.getText().toString().trim().length()<10){
+                } else if (contact.getText().toString().trim().length() < 10) {
                     contact.setError("Valid Contact No. Required");
-                }
-                else if (password.getText().toString().trim().equals("")) {
+                } else if (password.getText().toString().trim().equals("")) {
                     password.setError("Password Required");
                 } else if (password.getText().toString().trim().length() < 6) {
                     password.setError("Min. 6 Char Password Required");
-                }else if (confirmPassword.getText().toString().trim().equals("")) {
+                } else if (confirmPassword.getText().toString().trim().equals("")) {
                     confirmPassword.setError("Confirm Password Required");
                 } else if (confirmPassword.getText().toString().trim().length() < 6) {
                     confirmPassword.setError("Min. 6 Char Confirm Password Required");
-                }
-                else if(!confirmPassword.getText().toString().trim().matches(password.getText().toString().trim())){
+                } else if (!confirmPassword.getText().toString().trim().matches(password.getText().toString().trim())) {
                     confirmPassword.setError("Password Does Not Match");
+                } else if (gender.getCheckedRadioButtonId() == -1) {
+                    new CommonMethod(SignupActivity.this,"Please Select Gender");
+                }
+                else if(sCity.equals("")){
+                    new CommonMethod(SignupActivity.this,"Please Select City");
+                }
+                else if(dob.getText().toString().trim().equals("")){
+                    dob.setError("Please Select Date of Birth");
                 }
                 else {
                     System.out.println("Signup Successfully");
